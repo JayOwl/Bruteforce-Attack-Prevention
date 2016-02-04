f9c1daae6233432460a0017a3bcf0d1295109e37
Joel Richard Benoit <joel.r.benoit@gmail.com>
2016-02-03 20:54:35 -0800

    Brute Force Prevention
    A brute force attack is a repeated login attempt with a password and
    user library to guess a login and password combination.

---
+++ websecure2/Global.asax.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Threading;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace websecure2
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AntiForgeryConfig.SuppressIdentityHeuristicChecks = true;
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+        void Application_PostAuthenticateRequest()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var name = User.Identity.Name; // Get current user name. 
+
+                Security1Entities context = new Security1Entities();
+                var user = context.AspNetUsers.Where(u => u.UserName == name).FirstOrDefault();
+                IQueryable<string> roleQuery = from u in context.AspNetUsers
+                                               from r in u.AspNetRoles
+                                               where u.UserName == Context.User.Identity.Name
+                                               select r.Name;
+                string[] roles = roleQuery.ToArray();
+
+                HttpContext.Current.User = Thread.CurrentPrincipal =
+                                           new GenericPrincipal(User.Identity, roles);
+            }
+        }
+    }
+
+}
